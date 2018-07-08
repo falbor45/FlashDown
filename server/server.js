06715c4d91b62a3ce721b3472d68600cdcfc6fc0
Artur Falborski <falborski.artur@gmail.com>
2018-07-08 15:04:38 +0200

    fetch currently played match data by summoner name and region

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const app = require('express')();
-const http = require('http').Server(app);
+const fetch = require('node-fetch');
+const API_KEY = '';
 
 app.get('/', function(req, res){
   res.send('foo')
 });
 
-http.listen(3000, function(){
+app.get('/create-watcher/:leagueServer/:summonerName', (req, res) => {
+  fetch(`https://${req.params.leagueServer}.api.riotgames.com/lol/summoner/v3/summoners/by-name/${req.params.summonerName}?api_key=${API_KEY}`)
+    .then(response => response.json())
+    .then(response => fetch(`https://eun1.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${response.id}?api_key=${API_KEY}`)
+      .then(response => response.json())
+      .then(text => res.send(text))
+      .catch(error => res.send(error)))
+    .catch(error => res.send(error))
+});
+
+app.listen(3000, function(){
   console.log('listening on *:3000');
 });
