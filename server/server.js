047d7f13a6bd3811e833c3d6cb8475be3e9c5c91
Artur Falborski <falborski.artur@gmail.com>
2018-07-15 18:36:05 +0200

    encode summoner's watcher URL path

--- server/server.js
+++ server/server.js
@@ -4,7 +4,17 @@ const API_KEY = '';
 
 let arr = [];
 
-app.get('/', function(req, res){
+
+// encoding function below serves only as an example
+let encode = string => {
+  let newString = "";
+  for (let i = 0; i < string.length; i++) {
+    newString += string[i].charCodeAt();
+  }
+  return newString;
+}
+
+app.get('/', function (req, res) {
   res.send('foo')
 });
 
@@ -16,21 +26,20 @@ app.get('/create-watcher/:leagueServer/:summonerName', (req, res) => {
     .then(response => {
       if (arr.includes(response.id)) {
         res.send('Whoops, it looks like this summoner tracker is already up and running!')
-      } else {
-        arr.push(response.id);
-        console.log(`watcher for ${summonerName} has been created`)
-        app.get(`/watchers/${summonerName}`, (req, res) => {
-          fetch(`https://${leagueServer}.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${response.id}?api_key=${API_KEY}`)
-            .then(response => response.json())
-            .then(text => res.send(text))
-            .catch(error => res.send(error))
-        });
-        res.send(`Watcher has been created. Go to /watchers/${req.params.summonerName}`)
+        return null;
       }
+      arr.push(response.id);
+      app.get(`/watchers/${encode(summonerName)}`, (req, res) => {
+        fetch(`https://${leagueServer}.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${response.id}?api_key=${API_KEY}`)
+          .then(response => response.json())
+          .then(text => res.send(text))
+          .catch(error => res.send(error))
+      });
+      res.send(`Watcher has been created. Go to /watchers/${encode(req.params.summonerName)}`)
     })
     .catch(error => res.send(error))
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log('listening on *:3000');
 });
