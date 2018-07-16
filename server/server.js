ab7699c0bfa2602ac29b01c7007ef5039cf39b70
Artur Falborski <falborski.artur@gmail.com>
2018-07-16 09:45:00 +0200

    shorten received game detail's .json file

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.get('/', function (req, res) {
 app.get('/create-watcher/:leagueServer/:summonerName', (req, res) => {
   let leagueServer = req.params.leagueServer;
   let summonerName = req.params.summonerName;
+  let summonerId;
   fetch(`https://${leagueServer}.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summonerName}?api_key=${API_KEY}`)
     .then(response => response.json())
     .then(response => {
@@ -28,14 +29,39 @@ app.get('/create-watcher/:leagueServer/:summonerName', (req, res) => {
         res.send('Whoops, it looks like this summoner tracker is already up and running!')
         return null;
       }
-      arr.push(response.id);
+      summonerId = response.id;
+      arr.push(summonerId);
       app.get(`/watchers/${encode(summonerName)}`, (req, res) => {
-        fetch(`https://${leagueServer}.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${response.id}?api_key=${API_KEY}`)
+        fetch(`https://${leagueServer}.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${summonerId}?api_key=${API_KEY}`)
           .then(response => response.json())
-          .then(text => res.send(text))
+          .then(json => res.send({
+              mapId: json.mapId,
+              gameType: json.gameType,
+              gameQueueConfigId: json.gameQueueConfigId,
+              participants: json.participants.map(e => {
+                return {
+                  teamId: e.teamId,
+                  championId: e.championId,
+                  summonerName: e.summonerName,
+                  perks: e.perks,
+                  spell1: {
+                    id: e.spell1Id,
+                    used: new Date().getTime()
+                  },
+                  spell2: {
+                    id: e.spell2Id,
+                    used: new Date().getTime()
+                  }
+                }
+              }),
+              platformId: json.platformId,
+              bannedChampions: json.bannedChampions,
+              gameStartTime: json.gameStartTime,
+              gameLength: json.gameLength
+            }))
           .catch(error => res.send(error))
       });
-      res.send(`Watcher has been created. Go to /watchers/${encode(req.params.summonerName)}`)
+      res.send(`Watcher has been created. Go to /watchers/${encode(summonerName)}`)
     })
     .catch(error => res.send(error))
 });
