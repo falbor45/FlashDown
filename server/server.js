596a8af00c4029b49200b4e280b91a208637985a
Artur Falborski <falborski.artur@gmail.com>
2018-07-20 17:18:25 +0200

    allow updating summoner's spells cooldowns through POST method

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const app = require('express')();
 const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
 const API_KEY = '';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 let arr = [];
 
@@ -40,6 +44,18 @@ let mapSummonerSpellId = (data, spellId) => {
   }
 };
 
+// data argument is the data that comes from gameData variable
+let updateSummonerSpellCD = (data, summonerName, spell) => {
+  let newData = data;
+  let whichSpell = spell === '1' ? 'spell1' : 'spell2';
+  for (let i = 0; i < newData.participants.length; i++) {
+    if (newData.participants[i].summonerName === summonerName) {
+      newData.participants[i][whichSpell].used = new Date().getTime();
+    }
+  }
+  return newData;
+};
+
 app.get('/', function (req, res) {
   res.send('foo')
 });
@@ -57,30 +73,44 @@ app.get('/create-watcher/:leagueServer/:summonerName', (req, res) => {
       }
       summonerId = response.id;
       arr.push(summonerId);
-      app.get(`/watchers/${encode(summonerName)}`, (req, res) => {
-        fetch(`https://${leagueServer}.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${summonerId}?api_key=${API_KEY}`)
-          .then(response => response.json())
-          .then(json => res.send({
-              mapId: json.mapId,
-              gameType: json.gameType,
-              gameQueueConfigId: json.gameQueueConfigId,
-              participants: json.participants.map(e => {
-                return {
-                  teamId: e.teamId,
-                  championId: e.championId,
-                  summonerName: e.summonerName,
-                  perks: e.perks,
-                  spell1: mapSummonerSpellId(summonerSpells.data, e.spell1Id),
-                  spell2: mapSummonerSpellId(summonerSpells.data, e.spell2Id)
-                }
-              }),
-              platformId: json.platformId,
-              bannedChampions: json.bannedChampions,
-              gameStartTime: json.gameStartTime,
-              gameLength: json.gameLength
-            }))
-          .catch(error => res.send(error))
-      });
+      let gameData;
+      fetch(`https://${leagueServer}.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${summonerId}?api_key=${API_KEY}`)
+        .then(response => response.json())
+        .then(json => {
+          gameData = {
+            mapId: json.mapId,
+            gameType: json.gameType,
+            gameQueueConfigId: json.gameQueueConfigId,
+            participants: json.participants.map(e => {
+              return {
+                teamId: e.teamId,
+                championId: e.championId,
+                summonerName: e.summonerName,
+                perks: e.perks,
+                spell1: mapSummonerSpellId(summonerSpells.data, e.spell1Id),
+                spell2: mapSummonerSpellId(summonerSpells.data, e.spell2Id)
+              }
+            }),
+            platformId: json.platformId,
+            bannedChampions: json.bannedChampions,
+            gameStartTime: json.gameStartTime,
+            gameLength: json.gameLength
+          }
+        })
+        .catch(error => res.send(error))
+
+      app.route(`/watchers/${encode(summonerName)}`)
+        .get((req, res) => {
+          res.send(gameData)
+        })
+        .post((req, res) => {
+          if (req.body.action === 'updateSummonerSpell') {
+            gameData = updateSummonerSpellCD(gameData, req.body.summonerName, req.body.spell);
+            res.send(`${req.body.summonerName}'s spell has been updated!`)
+            return null;
+          }
+          res.send('No action specified!')
+        });
       res.send(`Watcher has been created. Go to /watchers/${encode(summonerName)}`)
     })
     .catch(error => res.send(error))
