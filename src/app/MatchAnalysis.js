c55d420b91af0c5569c112fb6fa6959e571f297a
Artur Falborski <falborski.artur@gmail.com>
2018-10-10 18:14:03 +0200

    move champion toggling element inside MatchAnalysis component

--- src/app/MatchAnalysis.js
+++ src/app/MatchAnalysis.js
@@ -1,29 +1,65 @@
 import React, { Component } from 'react'
 import MatchAnalysisItem from './MatchAnalysisItem'
+import { removeArrayElement } from './Helpers';
 import './MatchAnalysis.css'
 
 
 
 export default class MatchAnalysis extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedChampions: [1]
+    };
+  }
+
+  toggleChampionSelect = participantId => {
+    if (this.state.selectedChampions.includes(participantId)) {
+      this.setState({
+        selectedChampions: removeArrayElement(this.state.selectedChampions, participantId)
+      });
+      return null;
+    }
+    let newArray = this.state.selectedChampions;
+    newArray.push(participantId);
+    this.setState({
+      selectedChampions: newArray
+    });
+    return null;
+  };
+
   render() {
     return (
-      <div className="champion-stats">
-        <MatchAnalysisItem title="Vision score"
-                           allParticipants={this.props.allParticipants}
-                           selectedChampions={this.props.selectedChampions}
-                           calculatedProperties={["visionScore"]}/>
-        <MatchAnalysisItem title="Damage dealt to champions"
-                           allParticipants={this.props.allParticipants}
-                           selectedChampions={this.props.selectedChampions}
-                           calculatedProperties={["totalDamageDealtToChampions"]}/>
-        <MatchAnalysisItem title="Gold earned"
-                           allParticipants={this.props.allParticipants}
-                           selectedChampions={this.props.selectedChampions}
-                           calculatedProperties={["goldEarned"]}/>
-        <MatchAnalysisItem title="Creep score"
-                           allParticipants={this.props.allParticipants}
-                           selectedChampions={this.props.selectedChampions}
-                           calculatedProperties={["neutralMinionsKilled", "totalMinionsKilled"]}/>
+      <div>
+        <div className="champion-select">
+          {
+            this.props.allParticipants.map(e =>
+              <div onClick={() => this.toggleChampionSelect(e.participantId)}>
+                <img className={`${this.state.selectedChampions.includes(e.participantId) ? 'selected' : ''}`}
+                     src={e.champion.iconURL}/>
+              </div>
+            )
+          }
+        </div>
+        <div className="champion-stats">
+          <MatchAnalysisItem title="Vision score"
+                             allParticipants={this.props.allParticipants}
+                             selectedChampions={this.state.selectedChampions}
+                             calculatedProperties={["visionScore"]}/>
+          <MatchAnalysisItem title="Damage dealt to champions"
+                             allParticipants={this.props.allParticipants}
+                             selectedChampions={this.state.selectedChampions}
+                             calculatedProperties={["totalDamageDealtToChampions"]}/>
+          <MatchAnalysisItem title="Gold earned"
+                             allParticipants={this.props.allParticipants}
+                             selectedChampions={this.state.selectedChampions}
+                             calculatedProperties={["goldEarned"]}/>
+          <MatchAnalysisItem title="Creep score"
+                             allParticipants={this.props.allParticipants}
+                             selectedChampions={this.state.selectedChampions}
+                             calculatedProperties={["neutralMinionsKilled", "totalMinionsKilled"]}/>
+        </div>
       </div>
     )
   }
--- src/app/MatchDetails.js
+++ src/app/MatchDetails.js
@@ -1,48 +1,12 @@
 import React, { Component } from 'react'
 import './MatchDetails.css'
-import { removeArrayElement } from './Helpers';
 import MatchAnalysis from './MatchAnalysis'
 
 export default class MatchDetails extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      selectedChampions: [1]
-    };
-  }
-
-  toggleChampionSelect = participantId => {
-    if (this.state.selectedChampions.includes(participantId)) {
-      this.setState({
-        selectedChampions: removeArrayElement(this.state.selectedChampions, participantId)
-      });
-      return null;
-    }
-    let newArray = this.state.selectedChampions;
-    newArray.push(participantId);
-    this.setState({
-      selectedChampions: newArray
-    });
-    return null;
-  };
-
-
-
   render() {
     return (
       <div className="match-details">
-        <div className="champion-select">
-          {
-            this.props.allParticipants.map(e =>
-            <div onClick={() => this.toggleChampionSelect(e.participantId)}>
-              <img className={`${this.state.selectedChampions.includes(e.participantId) ? 'selected' : ''}`} src={e.champion.iconURL}/>
-            </div>
-            )
-          }
-        </div>
-        <MatchAnalysis selectedChampions={this.state.selectedChampions}
-                       allParticipants={this.props.allParticipants}/>
+        <MatchAnalysis allParticipants={this.props.allParticipants}/>
       </div>
     )
   }
