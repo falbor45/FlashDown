27d717eb9cac25ca55cadeb5cdb5f587aea0d965
Artur Falborski <falborski.artur@gmail.com>
2018-10-21 20:57:53 +0200

    add favicon

--- src/Html.js
+++ src/Html.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import favicon from './app/assets/favicon.png'
 
 const Html = (props) => {
   return (
     <html>
     <head>
       <title>Flash Down</title>
+      <link rel="shortcut icon" type="image/png" href={`/${favicon}`}/>
       <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"/>
       <link rel="stylesheet" type="text/css" href="/styles.css"/>
     </head>
