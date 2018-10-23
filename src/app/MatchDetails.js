67f4241f499620acadb9684f083bcc53725e34e3
Artur Falborski <falborski.artur@gmail.com>
2018-10-23 21:50:34 +0200

    add delay when changing MatchDetails subcomponents

--- src/app/MatchDetails.js
+++ src/app/MatchDetails.js
@@ -9,22 +9,35 @@ export default class MatchDetails extends Component {
     super(props);
 
     this.state = {
-      displayedComponent: 'matchOverview'
+      displayedComponent: 'matchOverview',
+      componentTransitionTime: 700
     }
   }
+
+  changeComponent = component => {
+    this.setState({
+      displayedComponent: null
+    });
+    setTimeout(() => {
+      this.setState({
+        displayedComponent: component
+      })
+    }, this.state.componentTransitionTime)
+  };
+
   render() {
     return (
       <div className="match-details">
         <div className="match-details-selection">
           <button className={this.state.displayedComponent === 'matchOverview' ? 'active' : ''}
-                  onClick={() => this.setState({ displayedComponent: 'matchOverview'})}>Overview</button>
+                  onClick={() => this.changeComponent('matchOverview')}>Overview</button>
           <button className={this.state.displayedComponent === 'matchAnalysis' ? 'active' : ''}
-                  onClick={() => this.setState({ displayedComponent: 'matchAnalysis'})}>Analysis</button>
+                  onClick={() => this.changeComponent('matchAnalysis')}>Analysis</button>
         </div>
-        <CSSTransition in={this.state.displayedComponent === 'matchOverview'} timeout={250} classNames="match-details-selection-fade" unmountOnExit>
+        <CSSTransition in={this.state.displayedComponent === 'matchOverview'} timeout={this.state.componentTransitionTime} classNames="match-details-selection-fade" unmountOnExit>
           <MatchOverview match={this.props.match} allParticipants={this.props.allParticipants}/>
         </CSSTransition>
-        <CSSTransition in={this.state.displayedComponent === 'matchAnalysis'} timeout={250} classNames="match-details-selection-fade" unmountOnExit>
+        <CSSTransition in={this.state.displayedComponent === 'matchAnalysis'} timeout={this.state.componentTransitionTime} classNames="match-details-selection-fade" unmountOnExit>
           <MatchAnalysis allParticipants={this.props.allParticipants}/>
         </CSSTransition>
       </div>
