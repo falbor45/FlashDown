ad98acc7cd5fcfda7cf1ff47aa023a69fc07cd2a
Artur Falborski <falborski.artur@gmail.com>
2018-10-31 21:57:47 +0100

    show challenger list of searched region in Home component

--- src/app/Home.js
+++ src/app/Home.js
@@ -10,7 +10,9 @@ export default class Home extends Component {
     this.state = {
       region: 'na',
       searchValue: '',
-      regionSelectVisible: false
+      regionSelectVisible: false,
+      challengerPlayers: null,
+      showChallengerPlayers: false
     }
   }
 
@@ -31,11 +33,42 @@ export default class Home extends Component {
     this.props.history.push(`/summoner/${this.state.region}/${this.state.searchValue}`);
   };
 
+  toggleChallengersList = show => {
+      this.setState({
+        showChallengerPlayers: show
+      })
+  };
+
+  fetchChallengerPlayers = region => {
+    this.setState({
+      challengerPlayers: null
+    });
+    fetch(`http://${window.location.host}:3000/challengerPlayers/${region}`)
+      .then(response => response.json())
+      .then(json => this.setState({
+        challengerPlayers: json
+      }))
+      .then(() => console.log(this.state.challengerPlayers))
+  };
+
+  componentDidMount() {
+    this.fetchChallengerPlayers(this.state.region);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.region !== this.state.region) {
+      console.log(prevState, this.state)
+      this.fetchChallengerPlayers(this.state.region)
+    }
+  }
+
   render() {
     return (
       <div className="home-wrapper">
-        <form className="summoner-search-form" onSubmit={this.handleSubmit}>
-          <input className="search-input"
+        <form className="summoner-search-form"
+              onSubmit={this.handleSubmit}
+              onFocus={() => this.toggleChallengersList(true)}>
+          <input className={`search-input ${this.state.showChallengerPlayers ? 'search-input-dropdown' : ''}`}
                  type="text"
                  placeholder="Search for summoner here..."
                  value={this.state.searchValue}
@@ -44,6 +77,21 @@ export default class Home extends Component {
           <Link to={`/summoner/${this.state.region}/${this.state.searchValue}`}>
             <button className="find-summoner" type="button">GO!</button>
           </Link>
+          {
+            this.state.showChallengerPlayers && this.state.challengerPlayers !== null ?
+              <div className="challengers-list">
+                <p className="challengers-title">Best players of the region</p>
+                {
+                  this.state.challengerPlayers.entries
+                    .sort((a, b) => b.leaguePoints - a.leaguePoints)
+                    .map(e => (
+                      <div className="challenger-item" >
+                        <a href={`http://${window.location.host}/summoner/${this.state.region}/${e.playerOrTeamName.toLowerCase()}`}>{e.playerOrTeamName} ({e.leaguePoints} LP)</a>
+                      </div>
+                    ))
+                }
+              </div> : null
+          }
         </form>
         {
           this.state.regionSelectVisible ? <RegionSelect handleRegionSelect={this.handleRegionSelect}/> : null
